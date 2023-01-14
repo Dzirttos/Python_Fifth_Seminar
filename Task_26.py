2541ccd74d2623f9c97889591b58fbd879b7c871
Dizrtt <demonic2003@bk.ru>
2023-01-14 14:12:01 +0200

    Task 28 completed.

--- Task_26.py
+++ Task_26.py
@@ -1,11 +1,25 @@
+# Задача 26:
+# Напишите программу, которая на вход принимает два числа A и B,
+# и возводит число А в целую степень B с помощью рекурсии.
+
 import os
 os.system('cls')
 
+print('Какое число нужно возвести в степень?')
+x = float(input())
+print('А в какую степень нужно возвести?')
+y = float(input())
+
 
 def power(a, b):
-    if b == 0: return 1
+    if b == 0:
+        return 1
+    elif b < 0:
+        return 1 / a * power(a, b+1)
     elif b >= 1:
-        return a*power(a, b-1)
+        return a * power(a, b-1)
 
 
-print(power(3, 2))
+print(f'\033[32mЕсли число {round(x)} возвести в степень {round(y)},то получится {power(x, y)}.')
+
+
